4bcb25d4f50e6c53e968a1e0794321120ec8d547
Harel Cohen <harelcohen2006@gmail.com>
2024-12-12 16:26:06 +0200

    fixed wrong code - collapse

--- BaseCalculations/Collapse.py
+++ BaseCalculations/Collapse.py
@@ -5,7 +5,5 @@ class Collapse(Operator):
     def calculate(self, num1, num2=None):
         if num1 < 0:
             raise ValueError("cannot perform # on a negative number")
-        number = str(num1).replace(".", "")
-        while len(number) > 1:
-            numbers = str(sum(int(digit) for digit in numbers))
-        return int(number)
+        number = (str(num1).replace(".", ""))
+        return sum(int(digit) for digit in number)
