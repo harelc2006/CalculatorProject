abc54fb441f93853683b1d42ed3f088118d423ea
Harel Cohen <harelcohen2006@gmail.com>
2024-12-12 20:39:17 +0200

    added a minus trimmer file

---
+++ Trimmer.py
@@ -0,0 +1,19 @@
+def minusTrimmer(exp):
+    exp = exp.replace(" ", "")
+    result = []
+    i = 0
+    while i < len(exp):
+        if exp[i] == '-':
+            minus_count = 0
+            while i < len(exp) and exp[i] == '-':
+                minus_count += 1
+                i += 1
+            if minus_count % 2 == 1:
+                result.append('-')
+            else:
+                result.append('')
+        else:
+            result.append(exp[i])
+            i += 1
+    return ''.join(result)
+
